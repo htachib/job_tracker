46d74c1db2c7d84ec320fd20ea4031daa48489f8
htachib <ryanckulp@gmail.com>
2016-10-09 00:00:57 -0500

    look up POs by days overdue

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,18 @@
+class DashboardController < ApplicationController
+
+  def index
+    overdue_length = params[:overdue].to_i || 0
+    @title = "At least #{overdue_length} days late"
+
+    po_list = PurchaseOrder.all
+
+    @overdue = po_list.map do |po|
+      if po.status.include?("late")
+        status_message = po.status.split # => ["53", "days", "ahead"]
+        days = status_message[0].to_i
+        po if days > overdue_length
+      end
+    end.compact
+  end
+
+end
